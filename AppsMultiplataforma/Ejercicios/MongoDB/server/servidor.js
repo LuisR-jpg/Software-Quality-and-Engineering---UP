46d4cac579f6d3d6fe2bdc64b2c97b3c9e5abd7d
LuisR-jpg <laloro08@hotmail.com>
2023-10-06 21:07:11 -0500

    06 de Octubre

--- AppsMultiplataforma/Ejercicios/MongoDB/server/servidor.js
+++ AppsMultiplataforma/Ejercicios/MongoDB/server/servidor.js
@@ -1,4 +1,4 @@
-const {MongoClient} = require('mongodb');
+const {MongoClient, ObjectId} = require('mongodb');
 const express = require('express');
 const app = express();
 var cors = require('cors');
@@ -8,90 +8,77 @@ const bodyParser = require('body-parser');
 const uri = "mongodb://localhost:27017";
 const cliente = new MongoClient(uri);
 
-async function insertar(datos, res) {
-    try {
-        await cliente.connect();
-        const coleccion = cliente.db("lalito").collection("post");
-        await coleccion.insertOne(datos);
-        return res.status(201).json({ success: true });
-    } catch (error) {
-        return res.status(400).json({ success: error, error })
-    } finally { await cliente.close(); }
+async function insertar(datos,res) {
+	try {
+		await cliente.connect();
+		const coleccion = cliente.db("lalito").collection("post");
+		await coleccion.insertOne(datos);
+		return res.status(201).json({sucess:true});
+	} catch(error) { 
+		return res.status(400).json({sucess:false,error}); 
+	} finally { await cliente.close(); }
 }
-
-async function modificar(datosBusqueda, datos, res) {
-    try {
-        await cliente.connect();
-        const coleccion = cliente.db("lalito").collection("post");
-        await coleccion.updateMany(datosBusqueda, {$set:datos});
-        return res.status(201).json({ success: true });
-    } catch (error) {
-        return res.status(400).json({ success: error, error })
-    } finally { await cliente.close(); }
+async function modificar(idBusqueda,datos,res) {
+	try {
+		await cliente.connect();
+		const coleccion = cliente.db("lalito").collection("post");
+		const id = new ObjectId(idBusqueda);
+		delete datos._id;
+		await coleccion.updateMany({_id:id},{$set:datos});
+		return res.status(201).json({sucess:true});
+	} catch(error) { 
+		console.log(error);
+		return res.status(400).json({sucess:false,error}); 
+	} finally { await cliente.close(); }
 }
-
-async function eliminar(datosBusqueda, res) {
-    try {
-        await cliente.connect();
-        const coleccion = cliente.db("lalito").collection("post");
-        await coleccion.deleteMany(datosBusqueda);
-        return res.status(201).json({ success: true });
-    } catch (error) {
-        return res.status(400).json({ success: error, error })
-    } finally { await cliente.close(); }
+async function eliminar(datosBusqueda,res) {
+	try {
+		await cliente.connect();
+		const coleccion = cliente.db("lalito").collection("post");
+		await coleccion.deleteMany(datosBusqueda);
+		return res.status(201).json({sucess:true});
+	} catch(error) { 
+		return res.status(400).json({sucess:false,error}); 
+	} finally { await cliente.close(); }
 }
-
 async function buscar(datosBusqueda, res) {
-    try {
-        await cliente.connect();
-        const coleccion = cliente.db("lalito").collection("post");
-        const resultados = await coleccion.find(datosBusqueda).toArray();
-        console.log(resultados);
-        res.send(resultados)
-    }
-    catch(error){
-        return res.status(400).json({success: false,error});
-    }
-    finally {
-        await cliente.close();
-    }
+	try {
+		await cliente.connect();
+		const coleccion = cliente.db("lalito").collection("post");	
+		console.log(datosBusqueda);
+		const resultados = await coleccion.find(datosBusqueda).toArray();
+		return res.send(resultados);
+	} catch(error) { 
+		return res.status(400).json({sucess:false,error});
+	} finally {
+		await cliente.close();
+	}
 }
-
-
 app.use(cors());
+//app.use(express.json());
+//app.use(express.urlencoded({ extended:true }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended:true }));
 
-app.use(express.urlencoded({
-    extended:true
-}));
-
-app.post('/guardar', (req, res) => {
-    console.log(req.body);
-    insertar(req.body.datos, res);
+app.post('/guardar',(req, res) => {	
+	insertar(JSON.parse(req.body.datos),res);
 });
-app.post('/borrar', (req, res) => {
-    console.log(req.body);
-    eliminar(req.body.datosBusqueda, res);
+app.post('/borrar',(req, res) => {
+	eliminar(JSON.parse(req.body.datosBusqueda),res);
 });
-app.post('/modificar', (req, res) => {
-    console.log(req.body);
-    modificar(req.body.datosBusqueda, req.body.datos. res);
+app.post('/modificar',(req, res) => {
+	modificar(req.body.idBusqueda,JSON.parse(req.body.datos),res);
 });
-app.post('/buscar', (req, res) => {
-    console.log(req.body);
-    buscar(req.body.datosBusqueda, res);
+app.post('/buscar',(req, res) => {
+	buscar(req.body.datosBusqueda,res)
 });
-app.get('/buscar', (req, res) => {
-    console.log(req.body);
-    buscar({}, res);
+app.get('/buscar',(req, res) => {
+	buscar({},res)
 });
-    
 app.get('/', (req, res) => {
-    console.log(req.body);
-    res.send('Hola');
-});
+  res.send('Hola ....')
+})
 
 app.listen(port, () => {
-    console.log(`Holahola. My app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`)
 })
