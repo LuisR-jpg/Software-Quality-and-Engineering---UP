99cb872c34bec8f9a4ae550667cc46388d67f8d8
LuisR-jpg <laloro08@hotmail.com>
2024-01-29 23:15:22 -0600

    Add crypto codes

---
+++ Ciberseguridad/Code/Cif_Asimetrico/src/asim/Exercise.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package asim;
+
+/**
+ *
+ * @author lalor
+ */
+public class Exercise {
+    String txt = "Hola profe!";
+    String file_private = "C:\\Users\\lalor\\Documents\\Git\\Software-Quality-and-Engineering---UP\\Ciberseguridad\\rsa.pri";
+
+}
